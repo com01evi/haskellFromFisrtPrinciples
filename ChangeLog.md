178889700be03879414636f071464a7d2318cfae
Shun Yasumoto <vis.nt.rstd@gmail.com>
2019-02-26 18:55:18 +0900

    first commit

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for haskellFromFisrtPrinciples
+
+## Unreleased changes
---
+++ app/Main.hs
@@ -0,0 +1,9 @@
+module Main where
+
+import Lib
+
+main :: IO ()
+main = do 
+  print "Hello, world!"
+  print "Hello, again!"
+  someFunc

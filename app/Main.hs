a9bb477f51d9feb05a22fb8341b15aab16d0974d
Shun Yasumoto <vis.nt.rstd@gmail.com>
2019-03-05 17:54:23 +0900

    finished chapter4

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,11 @@ module Main where
 
 import Chapter2
 import Chapter3
+import Chapter4
 
 main :: IO ()
 main = do
   s <- getLine
   sayHello s
   helloWorld
+  print $ changeMood Blah
---
+++ src/Chapter4.hs
@@ -0,0 +1,30 @@
+module Chapter4
+    (
+      Mood(..)
+    , changeMood
+    , allAwesome
+    , awesome
+    , isPalindrome
+    ) where
+
+data Mood = Blah | Woot deriving(Show) 
+
+
+changeMood :: Mood -> Mood
+changeMood Blah = Woot
+changeMood Woot = Blah
+
+awesome = ["Papuchon", "curry", ":)"]
+also = ["Quake", "The Simons"]
+allAwesome = [awesome, also]
+
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome = (==) <$> id <*> reverse
+
+myabs :: Integer -> Integer
+myabs a
+  | a < 0 = negate a
+  | otherwise = a
+
+f :: (a,b) -> (c,d) -> ((b,d), (a,c))
+f t1 t2 = ((snd t1,snd t2), (fst t1, fst t2))

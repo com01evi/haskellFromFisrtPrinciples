2b1430438bed8f78ae37ae5e8f079989888e7990
Shun Yasumoto <vis.nt.rstd@gmail.com>
2019-03-11 17:46:01 +0900

    learning type class

--- src/Chapter6.hs
+++ src/Chapter6.hs
@@ -1,5 +1,6 @@
 module Chapter6
     ( TisAnInteger(..) 
+    , EitherOr(..)
     ) where
 
 
@@ -7,3 +8,15 @@ data TisAnInteger = TisAn Integer
 
 instance Eq TisAnInteger where
   (TisAn x) == (TisAn y) = x == y
+
+data EitherOr a b = Hello a | GoodBye b
+
+instance (Eq a,Eq b) => Eq (EitherOr a b) where
+  Hello a == Hello b = a == b
+  GoodBye a == GoodBye b = a == b
+  Hello _ == GoodBye _ = False
+  GoodBye _ == Hello _ = False
+
+dividedThenadd :: (Fractional a) => a -> a -> a
+dividedThenadd x y = x / y + 1
+

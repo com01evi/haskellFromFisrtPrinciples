d3168a136d41fdce51c7a88bc418f6637aa14e36
Shun Yasumoto <vis.nt.rstd@gmail.com>
2019-08-18 22:24:26 +0900

    add f

--- src/Chapter8.hs
+++ src/Chapter8.hs
@@ -1,17 +1,40 @@
 module Chapter8
-    (
-    incTimes,
-    appryTimes
+    ( 
+      fact,
+      inc,
+      three',
+      applyTimes',
+      f
     ) where
 
-incTimes :: Int -> Int -> Int
-incTimes 0 n = n
-incTimes times n = 1 + incTimes (times -1) n
+fact :: Int -> Int
+fact n 
+  | n <= 0 = 1
+  | otherwise = n * fact (n-1)
 
-appryTimes :: (Eq a, Num a) => a -> (b -> b) -> b -> b
-appryTimes 0 f b = b 
-appryTimes n f b = f (appryTimes (n-1) f b)
+inc :: Num a => a -> a
+inc = (+1)
 
-appryTimes' :: (Eq a, Num a) => a -> (b -> b) -> b -> b
-appryTimes' 0 f b = b
-appryTimes' n f b = f . appryTimes' (n-1) f $ b
+three' = (inc . inc . inc) 0
+
+incTimes :: (Ord a, Num a) => a -> a -> a
+incTimes times n
+  | times <= 0 = n
+  | otherwise = 1 + (incTimes (times - 1) n)
+
+applyTimes :: (Ord a, Num a) => a -> (b -> b) -> b -> b
+applyTimes n f b 
+  | n <= 0 = b
+  | otherwise = f (applyTimes (n-1) f b)
+
+incTimes' :: (Ord a, Num a) => a -> a -> a
+incTimes' n b = applyTimes n (+1) b
+
+applyTimes' :: (Ord a, Num a) => a -> (b -> b) -> b -> b
+applyTimes' n f b 
+  | n <= 0 = b
+  | otherwise = f . applyTimes (n-1) f $ b
+
+f :: Bool -> Int
+f True = error "Blah"
+f False = 0

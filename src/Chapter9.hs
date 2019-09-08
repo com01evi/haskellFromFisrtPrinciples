2638216ed52e9b93e6cd149a31ee56d4c0718c0a
Shun Yasumoto <shunyasumoto@Shuns-MacBook-Air.local>
2019-09-08 23:06:44 +0900

    add mywords

--- src/Chapter9.hs
+++ src/Chapter9.hs
@@ -1,7 +1,11 @@
 module Chapter9
     ( 
     maybeTail,
-    maybeHead
+    maybeHead,
+    myenumFromTo,
+    mywords,
+    mydropWhile,
+    mytakeWhile
     ) where
 
 maybeTail :: [a] -> Maybe [a]
@@ -12,3 +16,24 @@ maybeTail (_:xs) = Just xs
 maybeHead :: [a] -> Maybe a
 maybeHead [] = Nothing
 maybeHead (x:xs) = Just x
+
+myenumFromTo :: (Enum a, Eq a) => a -> a-> [a]
+myenumFromTo i n
+  | i == n = [n]
+  | otherwise = i : myenumFromTo (succ i) n
+
+mywords :: String -> [String]
+mywords [] = []
+mywords str = mytakeWhile (' '==) str : mywords (mydropWhile (' '==) str)
+
+mydropWhile :: (a -> Bool) -> [a] -> [a]
+mydropWhile  _ [] = [] 
+mydropWhile p (x:xs)
+  | p x = xs
+  | otherwise = mydropWhile p xs
+
+mytakeWhile :: (a -> Bool) -> [a] -> [a]
+mytakeWhile _ [] = []
+mytakeWhile p (x:xs) 
+  | p x = []
+  | otherwise = x: mytakeWhile p xs

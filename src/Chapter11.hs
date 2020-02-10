4cc79e8034d9ff19ca63646c2d8b44f830fa1512
Shun Yasumoto <vis.nt.rstd@gmail.com>
2020-02-10 15:06:08 +0900

    start exercise: Vehicles

--- src/Chapter11.hs
+++ src/Chapter11.hs
@@ -4,3 +4,18 @@ module Chapter11
     ) where
 
 data MyBool = MyTrue | MyFalse
+
+data Doggies a = Husky a | Mastiff a deriving(Eq, Show)
+
+data Price = Price Integer deriving(Eq, Show)
+
+data Manufacturer = Mini | Mazda | Tata deriving(Eq, Show)
+
+data Airline = PapuAir | CatapultsR'Us | TakeYourChancesUnited deriving(Eq, Show)
+
+data Vehicles = Car Manufacturer Price | Plane Airline deriving(Eq, Show)
+
+myCar = Car Mini (Price 14000)
+urCar = Car Mazda (Price 20000)
+clownCar = Car Tata (Price 7000)
+doge = Plane PapuAir

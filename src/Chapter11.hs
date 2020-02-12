80d8a055d3b50fbabf08d063234cbd23742d9e67
Shun Yasumoto <vis.nt.rstd@gmail.com>
2020-02-12 12:00:42 +0900

    finished Exercise: For Example

--- src/Chapter11.hs
+++ src/Chapter11.hs
@@ -1,6 +1,7 @@
 module Chapter11
     (
-    MyBool
+    MyBool,
+    Example(MakeExample)
     ) where
 
 data MyBool = MyTrue | MyFalse
@@ -37,3 +38,11 @@ areCars = map isCar
 
 getManu :: Vehicle -> Manufacturer
 getManu (Car m p) = m
+
+data Example0 = Example0 deriving(Eq, Show)
+
+data Example1 = Example1 Int deriving(Eq, Show)
+
+data Example2 = Example2 Int String deriving(Eq, Show)
+
+data Example = MakeExample Int deriving(Show)

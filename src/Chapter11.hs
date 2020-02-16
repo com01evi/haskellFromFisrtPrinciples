daa945fffbbcdf3968504a699208024c4b055a1e
Shun Yasumoto <vis.nt.rstd@gmail.com>
2020-02-16 15:34:42 +0900

    finished page 655

--- src/Chapter11.hs
+++ src/Chapter11.hs
@@ -54,7 +54,7 @@ data Example = MakeExample Int deriving(Show)
 
 newtype Goat = Goat Int deriving(Eq, Show, TooMany)
 
-newtype Cow = Cow Int deriving(Eq, Show)
+--newtype Cow = Cow Int deriving(Eq, Show)
 
 newtype TupleIntString = TupleIntString (Int,String) deriving(Eq, Show)
 
@@ -124,3 +124,82 @@ data Garden = Gardenia Gardener
             | Rose Gardener
             | Lilac Gardener
             deriving(Show)
+
+--
+data GuessWhat = Chickenbutt deriving(Eq, Show)
+
+data Id a = MkId a deriving(Eq, Show)
+
+data Product a b = Product a b deriving(Eq, Show)
+
+data Sum a b = First a | Second b deriving(Eq, Show)
+
+data RecordProduct a b = RecordProduct{
+                             first :: a,
+                             second :: b
+                         }deriving(Eq, Show)
+
+newtype NumCow = NumCow Int deriving(Eq, Show)
+
+newtype NumPig = NumPig Int deriving(Eq, Show)
+
+data FarmHouse = FarmHouse NumCow NumPig deriving(Eq, Show)
+
+type FarmHouse' = Product NumCow NumPig
+
+newtype NumSheep = NumSheep Int deriving(Eq, Show)
+
+data BigFarmHouse = BigFarmHouse NumCow NumPig NumSheep deriving(Eq, Show)
+
+type BigFarmHouse' = Product NumCow (Product NumPig NumSheep)
+
+type Name = String
+
+type Age = Int
+
+type LovesMud = Bool
+
+type PoundsOfWool = Int
+
+data CowInfo = CowInfo Name Age deriving(Eq, Show)
+
+data PigInfo = PigInfo Name Age LovesMud deriving(Eq, Show)
+
+data SheepInfo = SheepInfo Name Age PoundsOfWool deriving(Eq, Show)
+
+data Animal = Cow CowInfo
+            | Pig PigInfo
+            | Sheep SheepInfo
+            deriving(Eq, Show)
+
+type Animal' = Sum CowInfo (Sum PigInfo SheepInfo)           
+
+bess' = (CowInfo "Bess" 4)
+
+bess = First bess' :: Animal'
+
+elmer' = Second (SheepInfo "elmer" 5 5)
+
+elmer = Second elmer' :: Animal'
+
+tom' = First (PigInfo "Tom" 5 True)
+
+tom = Second tom' :: Animal'
+
+trivialValue :: GuessWhat
+trivialValue = Chickenbutt
+
+idInt :: Id Int
+idInt = MkId 10
+
+type Awesome = Bool
+
+person :: Product Name Awesome
+person = Product "Simon" True
+
+data Twitter = Twitter deriving(Eq, Show)
+
+data AskFm = AskFm deriving(Eq, Show)
+
+socialnetwork :: Sum Twitter AskFm
+socialnetwork = First Twitter

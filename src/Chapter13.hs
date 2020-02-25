eb0935d9860c6b468c681dde0425eee8ff5ccb49
Shun Yasumoto <vis.nt.rstd@gmail.com>
2020-02-25 11:35:59 +0900

    init chapter13

---
+++ src/Chapter13.hs
@@ -0,0 +1,15 @@
+module Chapter13
+    (
+      helloWorld
+    , area
+    , rvrs
+    ) where
+
+helloWorld :: IO ()
+helloWorld = putStrLn "Hello, World!"
+
+area d = pi * r^2
+  where r = d/2
+
+rvrs :: String -> String
+rvrs = unwords . (foldl (flip (:)) []) . words

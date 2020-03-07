8d9838e81bda48a1c3c8aa8325eb0694ecb1569b
Shun Yasumoto <vis.nt.rstd@gmail.com>
2020-03-07 12:08:48 +0900

    finished by page 872

---
+++ src/Chapter14/Exercise.hs
@@ -0,0 +1,23 @@
+module Chapter14.Exercise
+    (
+    half,
+    halfIndentity,
+    listOrdered,
+    plusAssociative,
+    multAssociative
+    ) where
+
+half :: (Fractional a) => a -> a
+half x = x / 2
+
+halfIndentity = (*2) . half
+
+listOrdered :: (Ord a) => [a] -> Bool
+listOrdered xs = snd $ foldr go (Nothing, True) xs
+  where go _ status@(_,False) = status
+        go x (Nothing, t) = (Just x, t)
+        go x (Just y, t) = (Just x, y>=x)
+
+plusAssociative x y z = x + (y + z) == (x + y) + z
+
+multAssociative x y z = x * (y * z) == (x * y) * z

eb83605e003f43326d82f083ab129b53ed8d89bf
Shun Yasumoto <vis.nt.rstd@gmail.com>
2020-03-11 12:54:43 +0900

    end up to 903

--- src/Chapter15/List.hs
+++ src/Chapter15/List.hs
@@ -4,11 +4,14 @@ module Chapter15.List
     , list2
     , List(Nil,Cons)
     , append
+    , myProduct
+    , myHead
     ) where
 
 import Chapter11
+import Data.Monoid
 
-list = mappend  [1..3] [4..10]
+list = mappend [1..3] [4..10]
 
 list2 = mconcat [[1..3],[4..20]]
 
@@ -16,3 +19,13 @@ append :: List a -> List a -> List a
 append Nil list@(Cons x _) = list
 append list@(Cons x _) Nil = list
 append (Cons x list) list2 = Cons x (append list list2)
+
+sumResult = mappend (Sum 1) (Sum 2)
+
+myProduct :: [Int] -> Int
+myProduct = getProduct . mconcat . fmap Product
+
+fiftyfive = foldl mappend mempty ([1,10] :: [Sum Int])
+
+myHead :: [a] -> Maybe a
+myHead = getFirst . mconcat . fmap (First . Just)

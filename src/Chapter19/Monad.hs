c9abb0ef95aa0350675537ac37d8c6d897caea10
Shun Yasumoto <vis.nt.rstd@gmail.com>
2020-04-10 11:44:00 +0900

    finish chapter19

--- src/Chapter19/Monad.hs
+++ src/Chapter19/Monad.hs
@@ -2,7 +2,7 @@
 
 module Chapter19.Monad(
   shortyGen
- ,x
+ ,shortMain
 )where
 
 import Control.Monad(replicateM)
@@ -33,4 +33,63 @@ myreplicateM n m = do
 shortyGen :: IO String
 shortyGen = myreplicateM 7 $ randomElement alphaNum
 
-x = myreplicateM 4 [1,2,3]
+saveURI :: R.Connection -> BC.ByteString -> BC.ByteString -> IO (Either R.Reply R.Status)
+saveURI conn shortURI uri = R.runRedis conn $ R.set shortURI uri
+
+getURI :: R.Connection -> BC.ByteString -> IO (Either R.Reply (Maybe BC.ByteString))
+getURI conn shourtURI = R.runRedis conn $ R.get shourtURI
+
+linkShorty :: String -> String
+linkShorty shorty = concat [ "<a href=\""
+                           , shorty
+                           , "\">Copy and paste your shourt URL</a>"
+                           ]
+
+shortyCreated :: Show a => a -> String -> TL.Text
+shortyCreated resp shawty = TL.concat [ TL.pack (show resp)
+                                      , " shorty is: "
+                                      , TL.pack (linkShorty shawty)
+                                      ]
+
+shortyAintUri :: TL.Text -> TL.Text
+shortyAintUri uri = TL.concat [ uri
+                              , "wasn't a uri,"
+                              , "did you forget http://?"
+                              ]
+
+shortyFound :: TL.Text -> TL.Text
+shortyFound tbs = TL.concat [ "<a href=\""
+                            , tbs, "\">"
+                            , tbs, "</a>"
+                            ]
+
+app :: R.Connection -> ScottyM ()
+app rConn = do
+  get "/" $ do
+    uri <- param "uri"
+    let parsedUri :: Maybe URI 
+        parsedUri = parseURI (TL.unpack uri)
+    case parsedUri of
+      Just _ -> do
+        shawty <- liftIO shortyGen
+        let shorty = BC.pack shawty
+            uri' = encodeUtf8 (TL.toStrict uri)
+        resp <- liftIO (saveURI rConn shorty uri')
+        html (shortyCreated resp shawty)
+      Nothing -> text (shortyAintUri uri)
+
+  get "/:short" $ do
+    short <- param "short"
+    uri <- liftIO (getURI rConn short)
+    case uri of
+      Left reply -> text (TL.pack (show reply))
+      Right mbBS -> case mbBS of
+        Nothing -> text "uri not found"
+        Just bs -> html (shortyFound tbs)
+                   where tbs :: TL.Text
+                         tbs = TL.fromStrict (decodeUtf8 bs)
+
+shortMain :: IO ()
+shortMain = do
+  rConn <- R.connect R.defaultConnectInfo
+  scotty 3000 (app rConn)

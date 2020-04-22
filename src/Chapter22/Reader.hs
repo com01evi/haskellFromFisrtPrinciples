ef40c8a74d5e11617815698a32906dd10a05afb3
Shun Yasumoto <vis.nt.rstd@gmail.com>
2020-04-22 11:40:29 +0900

    Revert "finished chapter23"
    This reverts commit 2272ee786c5df9dda5a28140ecbc44634c14a4af.

--- src/Chapter22/Reader.hs
+++ src/Chapter22/Reader.hs
@@ -3,17 +3,10 @@ module Chapter22.Reader(
   fmapped,
   tupled,
   doubler,
-  getDogRM2,
-  x1,
-  x2,
-  x3,
-  toymain
+  getDogRM2
 )where
 
 import Data.Char
-import Data.Maybe
-import Data.Monoid
-import Control.Applicative
 
 cap :: [Char] -> [Char]
 cap xs = map toUpper xs
@@ -83,56 +76,3 @@ getDogRM2 = do
   name <- asks dogName
   a <- asks address
   return $ Dog name a
-
-x = [1..3]
-y = [4..6]
-z = [7..9]
-
-xs :: Maybe Integer
-xs = lookup 3 $ zip x y
-
-ys :: Maybe Integer
-ys = lookup 6 $ zip y z
-
-zs :: Maybe Integer
-zs = lookup 4 $ zip x y 
-
-z' :: Integer -> Maybe Integer
-z' n = lookup n $ zip x z
-
-x1 :: Maybe (Integer, Integer)
-x1 = (,) <$> xs <*> ys
-
-x2 :: Maybe (Integer, Integer)
-x2 = (,) <$> ys <*> zs
-
-x3 :: Integer -> Maybe (Integer, Integer)
-x3 n = (,) <$> z' n <*> z' n
-
-summed :: (Num a) => (a,a) -> a
-summed = uncurry (+)
-
-bolt :: Integer -> Bool
-bolt n = n>3 && n<8
-
-toymain :: IO ()
-toymain = do
-  print $ sequenceA [Just 3 , Just 2, Just 1]
-  print $ sequenceA [x, y]
-  print $ sequenceA [xs, ys]
-  print $ summed <$> x1
-  print $ fmap summed x2
-  print $ bolt 7
-  print $ fmap bolt z
-  print $ foldMap (All) $ sequA 7
-  print $ sequA $ fromMaybe 0 s'
-  print $ fmap sequA s'
-  print $ bolt $ fromMaybe 0 s'
-  print $ fmap bolt s'
-sequA :: Integral a => a -> [Bool]
-sequA n = sequenceA [(>3),(<8), even] n
-
-s' :: Maybe Integer
-s' = summed <$> x1
-
-

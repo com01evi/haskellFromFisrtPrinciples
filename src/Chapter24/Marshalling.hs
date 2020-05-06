b769f7637f8b826419f58a673109eadae0f7e21b
Shun Yasumoto <vis.nt.rstd@gmail.com>
2020-05-06 12:59:34 +0900

    finished chapter24

--- src/Chapter24/Marshalling.hs
+++ src/Chapter24/Marshalling.hs
@@ -6,13 +6,22 @@ module Chapter24.Marshalling(
   Host,
   Color,
   numlist,
-  NumberOrString,
+  NumberOrString(NOSS, NOSI),
   parseSemVer2,
   parseSemVer,
   parseRelease,
   parseRelease',
   parseMetadata,
-  parseMetadata'
+  parseMetadata',
+  parsePhone1,
+  parsePhone2,
+  parsePhone3,
+  parsePhone4,
+  parsePhone,
+  parseComment,
+  parseDate,
+  parsePlan,
+  parseSchedule
 )where
 
 import Control.Applicative
@@ -77,7 +86,7 @@ instance FromJSON Color where
                          Blue <$> v .: "blue" <|>
                          Yellow <$> v .: "yellow"
                    
-data NumberOrString = NOSS Int | NOSI String deriving(Eq, Show)
+data NumberOrString = NOSS Int | NOSI String deriving(Eq, Show, Read)
 
 instance FromJSON NumberOrString where
   parseJSON (Number i) = return $ NOSS (base10Exponent i)
@@ -131,3 +140,116 @@ parseMetadata' = do
 parseMetadata :: Parser Metadata
 parseMetadata = many parseMetadata'
 
+type NumberingPlanArea = Int
+type Exchange = Int
+type LineNumber = Int
+
+data PhoneNumber = PhoneNumber NumberingPlanArea Exchange LineNumber deriving(Eq, Show)
+
+parsePhone :: Parser PhoneNumber
+parsePhone = try parsePhone1 <|> try parsePhone2 <|> try parsePhone3 <|> try parsePhone4
+
+parsePhone1 :: Parser PhoneNumber
+parsePhone1 = do
+  char '('
+  npa <- int
+  char ')'
+  char ' '
+  exc <- int
+  char '-'
+  line <- int
+  return $ PhoneNumber npa exc line
+
+parsePhone2 :: Parser PhoneNumber
+parsePhone2 = do
+  char '1'
+  char '-'
+  parsePhone3
+
+
+parsePhone3 :: Parser PhoneNumber
+parsePhone3 = do
+  npa <- int
+  char '-'
+  exc <- int
+  char '-'
+  line <- int
+  return $ PhoneNumber npa exc line
+
+parsePhone4 :: Parser PhoneNumber
+parsePhone4 = do
+  list <- show <$> int
+  let npa =  read $ take 3 list :: Int
+  let nextList = drop 3 list
+  let exc = read $ take 3 nextList :: Int
+  let line = read $ drop 3 nextList :: Int
+  return $ PhoneNumber npa exc line
+
+data Schedule = Schedule Date [Plan] deriving(Eq, Show)
+
+data Date = Date Year Month Day deriving(Eq, Show)
+
+type Year = Int
+type Month = Int
+type Day = Int
+
+data Plan = Plan Time Activity deriving(Eq, Show)
+
+data Time = Time Hour Minute deriving(Eq, Show)
+
+type Hour = Int
+type Minute = Int
+
+type Activity = String
+
+parseComment :: Parser ()
+parseComment = do
+  char '-'
+  char '-'
+  skipMany $ char ' '
+  skipMany $ oneOf (['a'..'z'] ++ [' '])
+  skipMany newline
+  return ()
+
+myspace :: Parser ()
+myspace = skipMany (char ' ') <|> skipMany (char '\n')
+
+parseSchedule :: Parser Schedule
+parseSchedule = do
+  myspace
+  skipMany parseComment
+  myspace
+  date <- parseDate
+  plans <- some parsePlan
+  return $ Schedule date plans
+
+parseDate :: Parser Date
+parseDate = do
+  char '#'
+  char ' '
+  year <- int
+  char '-'
+  month <- int
+  char '-'
+  day <- int
+  skipMany $ char ' '
+  skipMany parseComment
+  skipMany newline
+  return $ Date year month day
+
+parsePlan :: Parser Plan
+parsePlan = do
+  time <- parseTime
+  char ' '
+  activity <- some $ oneOf $ ['a'..'z'] ++ ['A'..'Z'] ++ [' ']
+  skipMany $ char ' '
+  skipMany parseComment
+  skipMany newline
+  return $ Plan time activity
+
+parseTime :: Parser Time
+parseTime = do
+  hour <- int
+  char ':'
+  min <- int
+  return $ Time hour min

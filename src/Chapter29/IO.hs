95baed7c09c78bac1bb418e4e3c9b2f3f677e4e0
Shun Yasumoto <vis.nt.rstd@gmail.com>
2020-06-09 11:55:33 +0900

    end up to page 1785

--- src/Chapter29/IO.hs
+++ src/Chapter29/IO.hs
@@ -1,3 +1,23 @@
 module Chapter29.IO(
+  traceMain
 )where
 
+import Debug.Trace
+
+blah :: IO String
+blah = return "blah"
+
+blah' :: IO String
+blah' = trace "outer trace" blah
+
+woot :: IO String
+woot = return (trace "inner trace" "woot")
+
+traceMain :: IO ()
+traceMain = do
+  b <- blah'
+  putStrLn b
+  putStrLn b
+  w <- woot
+  putStrLn w
+  putStrLn w
